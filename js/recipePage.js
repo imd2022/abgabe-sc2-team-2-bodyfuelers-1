38bfdbdd4abea64ac2cdba559a192e2e4ed270fb
Theresa Husske <theresa.husske@t-online.de>
2023-06-20 13:26:44 +0200

    added a dropdown for save recipe button

--- js/recipePage.js
+++ js/recipePage.js
@@ -1,6 +1,32 @@
 function saveRecipe() {
-  document.getElementById("saveButton").style.borderRadius = "8px";
+  //document.getElementById("saveButton").style.borderRadius = "8px";
+  document.getElementById("saveButton").classList.toggle("save");
+  document.getElementById("saveDropdown").classList.toggle("show");
 }
+
+window.onclick = function (event) {
+  if (!event.target.matches(".special-iconbutton")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+
+  if (!event.target.matches(".special-iconbutton")) {
+    var dropdown = document.getElementsByClassName("special-iconbutton");
+    var i;
+    for (i = 0; i < dropdown.length; i++) {
+      var openDropdowns = dropdown[i];
+      if (openDropdowns.classList.contains("save")) {
+        openDropdowns.classList.remove("save");
+      }
+    }
+  }
+};
 
 /* getting a message from buddy by hovering over buddy */
 document.getElementById("buddy").addEventListener("mouseout", mouseOut);
